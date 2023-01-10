bc4a562acc65acb205e31bb8f5d1be1776854448
jospnunes <jospnunes@gmail.com>
2023-01-10 20:33:30 -0300

    Manipular Lista

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
-<script setup>
-
+<script>
+export default {
+  data(){
+    return{
+      show: true,
+      list:[1,2]
+    }
+  }
+}
 </script>
 
 <template>
- <p>olá mundo!!</p>
+ 
+ <button @click="list.push(list.length +1)" >Add um valor no final</button>
+ <button @click="list.pop()">Remover um valor do final</button>
+ <button @click="list.splice(list.length/2)">Remover metade dos valores</button>
+
+ <ul v-if="show && list.length">
+  <li v-for="item in list"> {{ item }}</li>
+ </ul>
 </template>
 
 <style>
