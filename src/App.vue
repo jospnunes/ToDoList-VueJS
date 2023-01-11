d25cbf976919116c92b5d331014ffc300a4d5f9c
jospnunes <jospnunes@gmail.com>
2023-01-10 21:25:28 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,56 @@ export default {
   data(){
     return{
       show: true,
-      list:[1,2]
+      list:[]
     }
   }
 }
 </script>
 
 <template>
- 
- <button @click="list.push(list.length +1)" >Add um valor no final</button>
- <button @click="list.pop()">Remover um valor do final</button>
- <button @click="list.splice(list.length/2)">Remover metade dos valores</button>
 
- <ul v-if="show && list.length">
-  <li v-for="item in list"> {{ item }}</li>
- </ul>
+<div id="flex">
+  <p v-if="list.length">A lista atualmente possui {{ list.length }} itens.</p>
+  <p v-else> Lista Vazia!</p>
+</div>
+
+<div id="flex">
+  <button @click="list.push(list.length +1)" >Add um valor no final</button>
+  <button @click="list.pop()">Remover um valor do final</button>
+  <button @click="list.splice(list.length/2)">Remover metade dos valores</button>
+  <input v-model="textInput" placeholder="Digite algo">
+  <button @click="list.push(textInput)">Add o texto ao lado na lista</button>
+</div>
+<div id="flex">
+  
+  <input type="number" :value="V" @change="setV">
+  <button v-if="v > 0" id="btnRmv">Remover o item {{ v }}</button>
+</div>
+
+<div id="flex">
+  <ul v-if="show && list.length">
+    <li  v-for="item in list"> {{ item }}</li>
+  </ul>
+</div>
+
+ 
 </template>
 
 <style>
+#flex{
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  
+}
 
+button{
+  width: 100px;
+  height: 45px;
+  border: 2px;
+  margin: 5px;
+}
+button:hover{
+  background-color: rgb(61, 19, 19);
+}
 </style>
