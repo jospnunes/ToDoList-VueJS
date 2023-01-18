2da9b61351ea5bba1e636f2df814447567bedd68
jospnunes <jospnunes@gmail.com>
2023-01-17 21:44:38 -0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
-<script>
-
+<script setup>
+import Todo from './Todo.vue';
 </script>
 
 <template>
 
+  <main>
+    
+    <Todo/>
 
+  </main>
 
- 
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: Roboto;
+  font-size: 25px;
+  color: rgb(8, 8, 8);
+}
+main{
+    width: 100%;
+    height: 100vh;
+    padding: 10px;
+    background-color: black;
+}
 
 </style>
---
+++ src/Todo.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="addContainer">
+          <p id="title">Adicione Sua Tarefa</p>
+          <input type="text" placeholder="Oque você vai fazer?"> 
+          <button class="btn" >Adicionar</button>
+          <button class="btn" >Mostrar</button>
+         </div>
+    </div>
+
+    <div class="taskContainer">
+            <h1>teste</h1>
+        </div>
+</template>
+
+<style>
+.container {
+    background: #fff;
+    width: 100%;
+    margin: 120px auto;
+    padding: 25px;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+#title{
+    font-size: 40px;
+    margin-right: 60px;
+}
+
+.addContainer{
+    padding: 1rem;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+}
+.btn{
+    width: 120px;
+    height: 40px;
+    margin: 10px;
+}
+.taskContainer{
+    display: flex;
+    justify-content: space-between;
+    background-color: rgb(255, 255, 255);
+}
+
+</style>
